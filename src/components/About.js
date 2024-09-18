760c669405e18785c1cc272d346935e4eb293f26
KAbirami <kabirami@student.tce.edu>
2024-09-18 16:26:03 +0530

    first commit

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ import { Container } from 'react-bootstrap';
 const AboutUs = () => {
   return (
     <Container className="my-4 text-center">
-      <h1>About Us</h1>
+      <h1>About </h1>
       <p>We are a company dedicated to providing the best services for our customers.</p>
       <p>Our mission is to innovate and lead in our field.</p>
     </Container>
